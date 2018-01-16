f35338d6ee854c51906db8fe1843e7801e0a85c7
Karol <karol.gratus@gmail.com>
2018-01-16 12:18:18 +0100

    app js render

--- client/App.js
+++ client/App.js
@@ -2,3 +2,16 @@ import React, { Component } from 'react';
 import io from 'socket.io-client';
 
 const socket = io('/');
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { users: [], messages: [], text: '', name: '' };
+  }
+
+  render() {
+    return this.state.name !== '' ? this.renderLayout() : this.renderUserForm();
+  }
+}
+
+export default App;
